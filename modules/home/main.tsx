7e6f4515ce30301792ca2edd5a6654843cbff8a3
levotaitri <taitri96@gmail.com>
2025-09-09 22:06:15 +0700

    fix: home page

--- modules/home/main.tsx
+++ modules/home/main.tsx
@@ -25,7 +25,7 @@ export function HomePage() {
         </div>
       </section>
 
-      <section className="max-w-7xl mx-auto px-4 xl:px-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
         <div className="relative">
           <Image
             src="https://theme.hstatic.net/200000349469/1001214457/14/slideshow_1.jpg?v=500"
@@ -36,11 +36,11 @@ export function HomePage() {
             priority
           />
           <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent"></div>
-          <div className="absolute bottom-16 left-12 text-white">
-            <span className="block text-sm font-medium mb-1">
+          <div className="absolute bottom-16 left-12">
+            <span className="block text-sm font-medium mb-1 text-white">
               Thảm len lông cừu
             </span>
-            <h3 className="text-xl font-bold mb-6">Chất lượng</h3>
+            <h3 className="text-xl font-bold mb-6 text-white">Chất lượng</h3>
             <Link
               className="inline-block bg-white px-8 py-2 text-sm font-medium border border-white hover:bg-transparent hover:text-white transition-colors uppercase"
               href="#"
@@ -59,11 +59,11 @@ export function HomePage() {
             priority
           />
           <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent"></div>
-          <div className="absolute bottom-16 left-12 text-white">
-            <span className="block text-sm font-medium mb-1">
+          <div className="absolute bottom-16 left-12">
+            <span className="block text-sm font-medium mb-1 text-white">
               Thiết kế hiện đại
             </span>
-            <h3 className="text-xl font-bold mb-6">100% Len</h3>
+            <h3 className="text-xl font-bold mb-6 text-white">100% Len</h3>
             <Link
               className="inline-block bg-white px-8 py-2 text-sm font-medium border border-white hover:bg-transparent hover:text-white transition-colors uppercase"
               href="#"
@@ -82,11 +82,11 @@ export function HomePage() {
             priority
           />
           <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent"></div>
-          <div className="absolute bottom-16 left-12 text-white">
-            <span className="block text-sm font-medium mb-1">
+          <div className="absolute bottom-16 left-12">
+            <span className="block text-sm font-medium mb-1 text-white">
               Tông màu nhã nhặn
             </span>
-            <h3 className="text-xl font-bold mb-6">Thảm lông</h3>
+            <h3 className="text-xl font-bold mb-6 text-white">Thảm lông</h3>
             <Link
               className="inline-block bg-white px-8 py-2 text-sm font-medium border border-white hover:bg-transparent hover:text-white transition-colors uppercase"
               href="#"
